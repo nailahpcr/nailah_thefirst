b6f3da01744ce82e96b77075757dc88b6094de46
nailahpcbr <nailah24si@siswa.pcr.ac.id>
2025-11-14 07:48:36 +0700

    seeder

---
+++ database/seeders/CreateFirstUser.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Foundation\Auth\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class CreateFirstUser extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'nailah@pcr.ac.id',
+            'password' => Hash::make('nailah')
+        ]);
+    }
+}
